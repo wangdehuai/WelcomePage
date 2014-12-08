a384d47590981eb881d3cf958b152e2952ee9e2c
wangdehuai <wangdehuai@me.com>
2014-12-08 12:24:09 +0800

    Initial Commit

---
+++ WelcomePage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WelcomePage
+//
+//  Created by wangdehuai on 14/12/8.
+//  Copyright (c) 2014年 wangdehuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WelcomePage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WelcomePage
+//
+//  Created by wangdehuai on 14/12/8.
+//  Copyright (c) 2014年 wangdehuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
